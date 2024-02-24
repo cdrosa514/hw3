2923d02b8f1f610067c5adde8db495665ee7f117
Christian De Rosa <christian.derosa@kellogg.northwestern.edu>
2024-02-24 21:01:40 +0000

    update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,26 @@ class EntriesController < ApplicationController
     # render entries/index view
   end
 
+  def show
+    # find an entry
+    @entry = Entry.find_by({ "id" => params["id"] })
+    # find Places associated with the entry
+    @places = Place.where({ "entry_id" => @entry["id"] })
+    # render entries/show view with details about Place
+  end
+
+  def new
+    @entry = Entry.find_by({ "id" => params["id"] })
+  end
+
+def create
+  @entry = Entry.new
+
+  @entry["title"] = params["title"]
+
+  @entry.save
+
+  redirect_to "/places#{entry["place_id"]}"
+end
+
 end
